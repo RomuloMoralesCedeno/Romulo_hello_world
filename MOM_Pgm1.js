787258334ee7624d03afa03faeb4595107ed1e97
RomuloMoralesCedeno <30451005+RomuloMoralesCedeno@users.noreply.github.com>
2017-08-19 10:18:27 -0600

    version 19 de agosto 2017

---
+++ MOM_Pgm1.js
@@ -0,0 +1,5 @@
+function welcomeWorld(){
+    window.alert("Welcome to the page.");   
+    document.write("<p>This is what <strong>matters</strong></p>");
+    console.log("Testing...");
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+var msg = "Hello, world!";
+alert(msg);
+alert(name);
+alert(typeof age);
+console.log("Hello, console!");
+document.write("Hello, document!");
+document.write(name);
